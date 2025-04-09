90b6dedf8bf5d666219719d415ec2ad00226b07e
Sidharthnaik003 <sidharthnaik003@gmail.com>
2025-04-09 11:22:12 +0530

    add

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -32,13 +32,13 @@ const Homepage = () => {
                                     Login
                                 </LightPurpleButton>
                             </StyledLink>
-                            <StyledLink to="/chooseasguest">
+                            {/* <StyledLink to="/chooseasguest">
                                 <Button variant="outlined" fullWidth
                                     sx={{ mt: 2, mb: 3, color: "#7f56da", borderColor: "#7f56da" }}
                                 >
                                     Login as Guest
                                 </Button>
-                            </StyledLink>
+                            </StyledLink> */}
                             <StyledText>
                                 Don't have an account?{' '}
                                 <Link to="/Adminregister" style={{color:"#550080"}}>
